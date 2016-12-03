b333f48e8181bfebcdd73ce1f5f4b7b294d287e0
githop <tom@githop.com>
2016-12-03 13:00:39 -0700

    feat - loading indicator on blog index

--- public/styles.b2328beb0372c051d06d.bundle.js
+++ public/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([4,5],{316:function(n,o){},828:function(n,o,c){n.exports=c(316)}},[828]);
+webpackJsonp([4,5],{317:function(n,o){},829:function(n,o,c){n.exports=c(317)}},[829]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map

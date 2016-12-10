45ef22d35eca3c2238024f8169d22da3bf30f485
githop <tom@githop.com>
2016-12-09 23:34:15 -0700

    new client build - various improvements

--- public/styles.b2328beb0372c051d06d.bundle.js
+++ public/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([4,5],{317:function(n,o){},829:function(n,o,c){n.exports=c(317)}},[829]);
+webpackJsonp([4,5],{318:function(n,o){},829:function(n,o,c){n.exports=c(318)}},[829]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map

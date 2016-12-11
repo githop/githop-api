a42d1018ec4c831940a1ae00476d120d02c2b71c
githop <tom@githop.com>
2016-12-11 13:44:35 -0700

    prefix routes with '/api/'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :paragraphs
-  resources :headers
-  resources :images
-  resources :articles
+
+  scope(:path => '/api') do 
+    resources :paragraphs
+    resources :headers
+    resources :images
+    resources :articles
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
